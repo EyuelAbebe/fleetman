973e18c5bc3a8bf7eeea90ab9cdbeb824532d664
eyuelabebe <eyueld@gmail.com>
2018-04-28 20:38:46 -0400

    added more sercurity for rest and webservice

--- src/main/java/com/virtualprogrammers/fleetman/config/SecurityConfiguration.java
+++ src/main/java/com/virtualprogrammers/fleetman/config/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.virtualprogrammers.fleetman.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,30 +13,50 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 @Profile("production")
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+public class SecurityConfiguration {
 
     //Authentication
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception{
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication()
                     .withUser("rac")
                     .password("secret")
                     .roles("USER", "ADMIN");
     }
 
+    //Authorization for api
+    @Configuration
+    @Order(2)
+    public static class ApiWebConfigurationAdapter extends WebSecurityConfigurerAdapter{
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .antMatcher("/**")
+                    .authorizeRequests()
+                        .anyRequest().hasRole("ADMIN")
+                        .and()
+                    .httpBasic()
+                    .and()
+                    .csrf()
+                    .disable();
+        }
+    }
+
     //Authorization
-    @Override
-    public void configure(HttpSecurity http) throws Exception{
-        http.httpBasic().disable();
-        http.antMatcher("/**").authorizeRequests()
+    @Configuration
+    @Order(1)
+    public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/website/**").authorizeRequests()
                     .anyRequest().hasRole("USER")
                     .and()
-                    .formLogin().loginPage("/login.jsp")
-                                .failureUrl("/login.jsp?error=1")
-                                .loginProcessingUrl("/login")
-                                .permitAll()
-                                .and()
-                                .logout()
-                                .logoutSuccessUrl("/website/vehicles/list.html");
+                    .formLogin().loginPage("/website/login.jsp")
+                    .failureUrl("/website/login.jsp?error=1")
+                    .loginProcessingUrl("/website/login")
+                    .permitAll()
+                    .and()
+                    .logout()
+                    .logoutSuccessUrl("/website/vehicles/list.html");
+        }
     }
 }
